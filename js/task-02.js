035c00e78afe9bac70740895a89e10d4ee0ef316
Mikhail <99561817+Mikhail-Zubko@users.noreply.github.com>
2024-01-13 18:20:36 +0200

    2nd tasks

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = (list) => {
+  return list.map((ingredient) => {
+    const listEl = document.createElement("li")
+    listEl.textContent = ingredient
+    listEl.classList.add('item')
+    return listEl
+  })
+}
+
+const ingredientsEl = ingredientsList(ingredients)
+const ingredientCollection = document.querySelector("#ingredients")
+ingredientCollection.append(...ingredientsEl)
+
+
+
